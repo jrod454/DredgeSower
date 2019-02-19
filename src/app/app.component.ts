71b3fa524348c21bc3c7973da5cf1a252542ab32
Jared Win Eaton <jared.eaton@ca.com>
2019-02-19 16:45:33 -0600

    Added simple import and display of imported data.

--- src/app/app.component.ts
+++ src/app/app.component.ts
@@ -1,4 +1,6 @@
 import { Component } from '@angular/core';
+import * as Papa from "node_modules/papaparse";
+import {p} from "@angular/core/src/render3";
 
 @Component({
   selector: 'app-root',
@@ -7,4 +9,30 @@ import { Component } from '@angular/core';
 })
 export class AppComponent {
   title = 'DredgeSower';
+  raw_data_file: any;
+
+  rows = [];
+
+  ngOnInit() {
+  }
+
+  setFile = (event: any) => {
+    this.raw_data_file = event.target.files[0];
+    Papa.parse(this.raw_data_file, {
+      complete: (data) => {
+        console.log(data.data);
+        data.data.forEach(person => {
+          if (person[0] !== null) {
+            this.rows.push([...person.slice(0,5), person[17]]);
+          }
+        });
+        this.rows = [...this.rows];
+        console.log(this.rows[6]);
+      }
+    });
+  };
+
+  sum = (total, number) => {
+    return total + number;
+  }
 }
